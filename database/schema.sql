1c3f0f5600486380fbdbc8f5756d8c3ff38e4155
Christoph Beger <christoph.beger@medizin.uni-leipzig.de>
2022-09-01 17:09:44 +0200

    Fix schema.sql

--- database/schema.sql
+++ database/schema.sql
@@ -6,13 +6,13 @@ CREATE TABLE subject (
 );
 
 CREATE TABLE assessment1 (
-    assessment_id bigint                  NOT NULL GENERATED ALWAYS AS IDENTITY,
-    subject_id    bigint                  NOT NULL REFERENCES subject,
-    created_at    timestamp with timezone NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    assessment_id bigint                   NOT NULL GENERATED ALWAYS AS IDENTITY,
+    subject_id    bigint                   NOT NULL REFERENCES subject,
+    created_at    timestamp with time zone NOT NULL DEFAULT CURRENT_TIMESTAMP,
     height        numeric,
     weight        numeric,
     PRIMARY KEY (assessment_id)
 );
 
-COPY subject (subject_id, birth_date, sex) FROM '/var/lib/postgresql/subject.csv' DELIMITER ',' CSV HEADER;
-COPY assessment1 (subject_id, created_at, height, weight) FROM '/var/lib/postgresql/assessment1.csv' DELIMITER ',' CSV HEADER;
+COPY subject (subject_id, birth_date, sex) FROM '/docker-entrypoint-initdb.d/subject.csv' DELIMITER ',' CSV HEADER;
+COPY assessment1 (subject_id, created_at, height, weight) FROM '/docker-entrypoint-initdb.d/assessment1.csv' DELIMITER ',' CSV HEADER;

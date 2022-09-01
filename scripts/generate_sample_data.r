ff32111256079924f6f338273efbb812258140c9
Christoph Beger <christoph.beger@medizin.uni-leipzig.de>
2022-09-01 16:20:44 +0200

    Update example data script

--- scripts/generate_sample_data.r
+++ scripts/generate_sample_data.r
@@ -1,30 +1,34 @@
 library(purrr)
+library(GrowthSDS)
+library(lubridate)
 
-n <- 10000
+n        <- 1000
 min_date <- as.Date("1950-01-01")
 max_date <- Sys.Date()
+ref      <- combineReferences(
+  GrowthSDS::kromeyerHauschild,
+  GrowthSDS::microcensus.germany,
+  x.cut = 18
+)
 
 subject <- data.frame(
   subject_id = 1:n,
   birth_date = sample(seq(min_date, max_date, by = "day"), n, TRUE),
-  sex        = sample(c("m", "f"), n, replace = TRUE)
+  sex        = sample(c("male", "female"), n, replace = TRUE)
 )
 
-assessment1 <- pmap_dfr(subjects, function(subject_id, birth_date, sex) {
+assessment1 <- pmap_dfr(subject, function(subject_id, birth_date, sex) {
   result <- data.frame()
   for (i in 1:sample(1:10, 1)) {
-    result <- rbind(
-      result,
-      data.frame(
-        subject_id = subject_id,
-        created_at = sample(seq(birth_date, max_date, by = "day"), 1),
-        height     = rnorm(1, ifelse(sex == "m", 180, 170), 10),
-        weight     = rnorm(1, 70, 20)
-      )
-    )
+    created_at <- sample(seq(birth_date, max_date, by = "day"), 1)
+    age        <- time_length(interval(birth_date, created_at), "years")
+    result     <- rbind(result, data.frame(subject_id, created_at, age, sex))
   }
   return(result)
 })
 
+assessment1$height <- round(centile(assessment1$age, rnorm(1, 0, 1), assessment1$sex, "height", ref), 1)
+assessment1$weight <- round(centile(assessment1$age, rnorm(1, 0, 1), assessment1$sex, "weight", ref), 3)
+
 write.csv(subject, "database/subject.csv", row.names = FALSE)
-write.csv(assessment1, "database/assessment1.csv", row.names = FALSE)
+write.csv(assessment1[, c(1, 2, 5, 6)], "database/assessment1.csv", row.names = FALSE)
